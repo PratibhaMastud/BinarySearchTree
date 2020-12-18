c740e2e34021234143ca879937eba604a8b58984
PratibhaMastud <karandepratibha999@gmail.com>
2020-12-18 17:36:29 +0530

    [Pratibha Mastud] Add: Create Binary Search Tree

---
+++ BST/BinarySearchTree/BinarySearchTree/BST.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTree
+{
+    public class BST<T> where T: IComparable<T>
+    {
+        public T NodeData { get; set; }
+        public BST<T> LeftTree { get; set; }
+        public BST<T> RightTree { get; set; }
+        public BST(T nodeData)
+        {
+            this.NodeData = nodeData;
+            this.LeftTree = null;
+            this.RightTree = null;
+        }
+
+        int LeftCount = 0, RightCount = 0;
+        bool result = false;
+
+        public void Insert(T item)
+        {
+            T currentNodeValue = this.NodeData;
+            if ((currentNodeValue.CompareTo(item)) > 0)
+            {
+                if (this.LeftTree == null)
+                    this.LeftTree = new BST<T>(item);
+                else
+                    this.LeftTree.Insert(item);
+            }
+            else
+            {
+                if (this.RightTree == null)
+                    this.RightTree = new BST<T>(item);
+                else
+                    this.RightTree.Insert(item);
+            }
+        }
+
+        public void Display()
+        {
+            if (this.LeftTree != null)
+            {
+                this.LeftCount++;
+                this.LeftTree.Display();
+            }
+            Console.Write(this.NodeData.ToString()+"-->");
+            if (this.RightTree != null)
+            {
+                this.RightCount++;
+                this.RightTree.Display();
+            }
+        }
+    }
+}
---
+++ BST/BinarySearchTree/BinarySearchTree/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome in Binary Search tree Problem");
+            BST<int> bST = new BST<int>(56);
+            bST.Insert(30);
+            bST.Insert(70);
+            bST.Display();
+        }
+    }
+}
